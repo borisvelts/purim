dff74d2539a48d16d6c62424e2e13556980417e3
shani-sit <sitton.shani@gmail.com>
2021-01-26 19:49:26 +0200

    first commit

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ router.get('/', (req, res) => {
     return res.send('hello world')
 })
 
+router.get('/wow',(req,res)=>{
+    return res.render('index')
+})
+router.get('/shani',(req,res)=>{
+    return res.render('shani')
+})
 module.exports = router;
