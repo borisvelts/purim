2a7e61adec504b27163f670922dbdcff90d234bd
alicecodeclub <alicecodeclub@gmail.com>
2021-01-27 09:55:12 +0200

    nastia test2

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,12 @@ const express = require("express");
 var router = express.Router();
 
 router.get('/', (req, res) => {
-<<<<<<< HEAD
     return res.render('index')
 
 })
 
 router.get('/nastia', (req, res) => {
     return res.render('nastia')
-=======
-    return res.render('inner/page') 
->>>>>>> 3c1f8fdb07392fb52ca554ca47bb48c0ec80b3f4
 })
 
 module.exports = router;
