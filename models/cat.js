89059ff86c533b7dd4aa28e58ada5eecbecb1479
IfatNeumann <ineumann19@gmail.com>
2021-02-12 15:49:20 +0000

    mongoose - hands on

---
+++ models/cat.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const CatSchema = new mongoose.Schema(
+    {
+        name: String,
+        age: Number,
+        owner: String
+
+    }
+);
+
+module.exports = mongoose.model("Cat", CatSchema);
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var Child = require('../models/child')
 var School = require('../models/school')
 var Picture = require('../models/picture')
+var Cat = require('../models/cat')
 
 router.get('/', async (req, res) => {
     var children = await Child.find({status:"APPROVED"}).populate('picture')
@@ -36,7 +37,40 @@ router.get('/end', (req, res) => {
     return res.render('shani/end')
 })
 
-router.get('/AliceCode', (req, res) => {
+router.get('/AliceCode', async (req, res) => {
+    // create
+    //Cat.create({name: "Heineken", age: 1})
+    //Cat.create({name: "Cappuchi", age: 2, owner: "Ifat"}) //<- Added owner
+
+    // find all
+    // var ifatsCats = await Cat.find()
+    // res.locals.ifatsCats = ifatsCats
+    // console.log(ifatsCats)
+
+    //find one + catch error
+    // Cat.findOne({"name": "Cappuchi"}, function (err, doc) {
+    //     if(err != null) { //error
+    //         return res.status(500).send('error fetching cat')
+    //     }
+    //     var cat = doc
+    //     console.log(cat)
+    //     return res.render('index')
+    // })
+
+
+    // print to screen: a.find all and initialize res.locals.cats
+    // var ifatsCats = await Cat.find()
+    // res.locals.cats = ifatsCats
+
+    // b. put that in the ejs file in order to see your list:
+    // <div class="row d-flex justify-content-around">
+    //     <% for(var i=0;i<cats.length;i++){ %>
+    //                 <p>
+    //                     <%=cats[i].name%>
+    //                 </p>
+    //         <%}%>
+    // </div>
+
     return res.render('index')
 })
 
