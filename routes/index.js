c86b0f6a6d2bcf6f89befa7871555d11285e6df3
Boris Velts <borisvelts@gmail.com>
2021-02-21 21:09:51 +0200

    mod

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,11 @@ const Dog = require("../models/dog");
 const Neighborhood = require('../models/neighborhood');
 
 
+router.get('/neighborhood', async (req, res) => {
+    res.locals.neighborhoods = Neighborhood.find();
+    res.locals.neighborhood = req.query.name;
+    return res.render('shani/neighborhood')
+})
 
 router.get('/hi', async (req, res) => {
     var children = await Child.find({ status: "APPROVED" }).populate('picture')
@@ -44,170 +49,16 @@ router.post('/upload', async (req, res) => {
     return res.redirect('/?firstName=' + child.firstName)
 })
 
-// router.get('/' , async (req,res)=>{
-//     // Child.create(name:"test"})
-//     var Children = await Child.find();
-//     console.log(children)
-//     return res.render('index')
-// })
-router.get('/nastia', (req, res) => {
-    return res.render('nastia')
-})
-
-router.get('/alisa', (req, res) => {
-    return res.render('alisa/alisa')
-})
-router.get('/ifat', (req, res) => {
-    return res.render('ifat')
-})
-router.get('/eden', (req, res) => {
-    return res.render('eden')
-})
-router.get('/shani', async (req, res) => {
-    //create
-    // Alien.create({name: "Bob", planet: "Mars"})
-    // Alien.create({name: "Marios", planet:"Noga", numOfEyes: 50}) //<- Added owner
-
-    // find all
-    //var ShaniAliens = await Alien.find()
-    //res.locals.ShanisAliens = ifatsCats
-    //console.log(ShaniAliens)
-
-    //find one + catch error
-    // await Alien.findOne({"name": "Bobo"}, function (err, doc) {
-    //      if(err != null) { //error
-    //         return res.status(500).send('error fetching cat')
-    //     }
-    //     var Alien = doc
-    //     console.log(Alien)
-    //     return res.render('index')
-    // })
-
-
-    // print to screen: a.find all and initialize res.locals.cats
-    //var ShaniAliens = await Alien.find()
-    //res.locals.aliens = ShaniAliens
-
-    // b. put that in the ejs file in order to see your list:
-    return res.render('shani')
-})
-router.get('/karin', (req, res) => {
-    return res.render('karin')
-})
 
 router.get('/end', (req, res) => {
     return res.render('shani/end')
 })
 
 router.get('/', async (req, res) => {
-    var neighborhoods= await Neighborhood.find()
+    var neighborhoods = await Neighborhood.find()
     res.locals.neighborhoods = neighborhoods
 
     return res.render('index')
 })
 
-
-router.get('/sync', (req, res) => res.render('sync'))
-
 module.exports = router;
-
-router.get('/eden2', async (req, res) => {
-
-    // create
-    Dog.create({ name: "dogOne", age: 6 })
-    Dog.create({ name: "dogTwo", age: 2, owner: "someone" }) //<- Added owner
-
-    //  var edensdog = await dog.find()
-    //  res.locals.edendog = edensdog
-    //  console.log(edensdog)
-
-
-    //find one + catch error
-    //  Cat.findOne({"name": "dogOne"}, function (err, doc) {
-    //      if(err != null) { //error
-    //          return res.status(500).send('error fetching dog')
-    //      }
-    //      var dog = doc
-    //      console.log(dog)
-    //      return res.render('index')
-    //  })
-
-
-    // print to screen: a.find all and initialize res.locals.cats
-   
-
-    // b. put that in the ejs file in order to see your list:
-    // <div class="row d-flex justify-content-around">
-    //     <% for(var i=0;i<neighborhoods.length;i++){ %>
-    //                 <p>
-    //                     <%=neighborhoods[i].name%>
-    //                 </p>
-    //         <%}%>
-    // // </div>
-    return res.render('eden')
-
-}
-)
-
-// // create
-// Dog.create({ name: "dogOne", age: 6 })
-// Dog.create({ name: "dogTwo", age: 2, owner: "someone" }) //<- Added owner
-
-
-// var edensdog = await dog.find()
-// res.locals.edendog = edensdog
-// console.log(edensdog)
-
-
-//find one + catch error
-//  Cat.findOne({"name": "dogOne"}, function (err, doc) {
-//      if(err != null) { //error
-//          return res.status(500).send('error fetching dog')
-//      }
-//      var dog = doc
-//      console.log(dog)
-//      return res.render('index')
-//  })
-
-
-router.get('/boris', async (req, res) => {
-    res.render('indexBoris')
-})
-
-router.get('/karin2', async (req, res) => {
-
-    // create
-    Dog.create({ name: "fishOne", age: 3 })
-    Dog.create({ name: "fishTwo", age: 1, owner: "someone" }) //<- Added owner
-
-    //  var edensdog = await dog.find()
-    //  res.locals.edendog = edensdog
-    //  console.log(edensdog)
-
-
-    //find one + catch error
-    //  Cat.findOne({"name": "dogOne"}, function (err, doc) {
-    //      if(err != null) { //error
-    //          return res.status(500).send('error fetching dog')
-    //      }
-    //      var dog = doc
-    //      console.log(dog)
-    //      return res.render('index')
-    //  })
-
-    // print to screen: a.find all and initialize res.locals.cats
-    //  var edensDog = await dog.find()
-    //  res.locals.dog = edensDog
-
-    // b. put that in the ejs file in order to see your list:
-    // <div class="row d-flex justify-content-around">
-    //     <% for(var i=0;i<cats.length;i++){ %>
-    //                 <p>
-    //                     <%=cats[i].name%>
-    //                 </p>
-    //         <%}%>
-    // // </div>
-
-    return res.render('karin');
-})
-
